f56797901ece2c02d99f7ae75a7f1e401e2a829a
timsurrealedu <timothy.darmawan@binus.ac.id>
2026-01-09 16:45:10 +0700

    fixed authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authenticateToken = require('../middleware/authMiddleware');
 
-// Public Routes (Anyone can access)
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// Protected Routes (Must have token)
 router.get('/profile', authenticateToken, authController.getProfile);
 
 module.exports = router;
